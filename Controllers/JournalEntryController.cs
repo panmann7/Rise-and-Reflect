0b6c3c0c4cc8aaaebbb5c18a8308932a4ba768cc
unknown <annlowe789@gmail.com>
2024-08-03 15:30:47 +1200

    create journal entry entity

---
+++ Controllers/JournalEntryController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JournalApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JournalEntryController : ControllerBase
+{
+
+}
+
---
+++ Entities/JournalEntry.cs
@@ -0,0 +1,7 @@
+namespace JournalApp.Entities;
+
+public class JournalEntry
+{
+    public required string Date { get; set; }
+    public required string Time { get; set; }
+}
